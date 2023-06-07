c66e3de955405d42845baaa4b91ff3f1217f81da
rahulmukh1ya <samueljackson649@gmail.com>
2023-06-07 10:04:35 +0545

    Form UI basic done

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: const LoginWidget(),
     );
   }
@@ -41,74 +38,77 @@ class _LoginWidgetState extends State<LoginWidget> {
           decoration: BoxDecoration(
             gradient: LinearGradient(
               colors: [
-                Colors.orange[800]!,
+                Colors.yellow[800]!,
                 Colors.orange[600]!,
               ],
-              begin: Alignment.topLeft,
-              end: Alignment.centerRight,
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
             ),
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: Stack(
+            alignment: Alignment.center,
             children: [
-              const Expanded(
-                flex: 3,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 36.0,
-                    horizontal: 24.0,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Expanded(
+                    flex: 3,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 36.0,
+                        horizontal: 24.0,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Register",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 45.0,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 45,
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Register",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 40.0,
-                          fontWeight: FontWeight.w500,
+                  Expanded(
+                    flex: 6,
+                    child: Container(
+                      width: double.infinity,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(25),
+                          topRight: Radius.circular(25),
                         ),
                       ),
-                      SizedBox(
-                        height: 10.0,
+                      child: const Padding(
+                        padding: EdgeInsets.all(24.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            TextField(),
+                          ],
+                        ),
                       ),
-                      // Text(
-                      //   "Please provide your details below",
-                      //   style: TextStyle(
-                      //     color: Colors.white,
-                      //     fontSize: 22.0,
-                      //     fontWeight: FontWeight.w400,
-                      //   ),
-                      // )
-                    ],
-                  ),
-                ),
+                    ),
+                  )
+                ], //children
               ),
-              Expanded(
-                flex: 6,
-                child: Container(
-                  width: double.infinity,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(25),
-                      topRight: Radius.circular(25),
-                    ),
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsets.all(24.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        TextField(),
-                      ],
-                    ),
-                  ),
-                ),
-              )
+              const Positioned(
+                  top: 230,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.grey,
+                    radius: 50,
+                  )),
             ],
           ),
         ),
